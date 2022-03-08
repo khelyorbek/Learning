29647090c74118d5b4668c69aeec5a907b933e4d
khelyorbek <khelyorbek@gmail.com>
2022-03-07 22:48:30 -0500

    23.1 SQLA Intro, learning patterns / schema config with Flask

--- 23_SQLAlchemy/23_1_SQLAlchemy_Intro/models.py
+++ 23_SQLAlchemy/23_1_SQLAlchemy_Intro/models.py
@@ -14,3 +14,22 @@ def connect_db(app):
 
 
 # MODELS GO BELOO! (Patterns / Schema)
+class Pet(db.Model):
+    """Pet."""
+    # Dunder for creating a table
+    __tablename___ = "pets"
+
+    # Changing the output that we get when an object is instantiated
+    def __repr__(self):
+        p = self
+        return f"<Pet id={p.id} name={p.name} species={p.species} hunger={p.hunger}>"
+
+    # Defining the columns
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50),nullable=False,unique=True)
+    species = db.Column(db.String(30),nullable=True, default=20)
+    breed = db.Column(db.String(30), nullable=True)
+    hunger = db.Column(db.Integer,nullable=False,default=20)
+
+# USING OUR MODEL
+# Ye BOII
