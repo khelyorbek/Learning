b6ecdd831c0aa823d003c0ef2cfe5a695790d790
Elyorbek Khudaybergenov <khelyorbek@gmail.com>
2024-03-28 19:21:56 -0400

    TS lesson 2 files

---
+++ 49_TypeScript/YT - Programming With Mosh/2_built_in_types/src/index.ts
@@ -0,0 +1,6 @@
+let sales: number = 123456789;
+let sales2: number = 123_456_789; // can do _ separation with big nums
+let course: string = "TypeScript";
+let is_published: boolean = true;
+
+let level;
